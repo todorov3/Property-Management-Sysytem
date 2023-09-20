539d810b6d1352f6e94e616e2d7f4428a6ec4460
todorov3 <yuliantodorovit@gmail.com>
2023-09-20 22:39:13 +0300

    Add UserApiController partial implementation

--- PropertyManagementSystem/PropertyManagementSystem/Controllers/UserApiController.cs
+++ PropertyManagementSystem/PropertyManagementSystem/Controllers/UserApiController.cs
@@ -1,6 +1,27 @@
-﻿namespace PropertyManagementSystem.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PropertyManagementSystem.Services.Contracts;
+
+namespace PropertyManagementSystem.Controllers
 {
-    public class UserController
+    [ApiController]
+    [Route("api/users")]
+    public class UserApiController : ControllerBase
     {
+        private readonly IUserService _userService;
+
+        public UserApiController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            return Ok(await _userService.GetAllUsers());
+        }
+
+
     }
 }
