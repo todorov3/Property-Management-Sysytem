8cadd26b814e0eccb9715714c325da7c83338a26
todorov3 <yuliantodorovit@gmail.com>
2023-09-26 16:18:38 +0300

    Add partial implementation of RequestApiController

---
+++ PropertyManagementSystem/PropertyManagementSystem/Controllers/RequestApiController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PropertyManagementSystem.Models.DTO;
+using PropertyManagementSystem.Services.Contracts;
+
+namespace PropertyManagementSystem.Controllers
+{
+    [ApiController]
+    [Route("api/requests")]
+    public class RequestApiController : ControllerBase
+    {
+        private readonly IRequestService _requestServce;
+
+        public RequestApiController(IRequestService requestService)
+        {
+            _requestServce = requestService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetRequestById(int id)
+        {
+            return Ok(await _requestServce.GetRequestById(id));
+        }
+
+        [HttpGet("{tenandId}")]
+        public async Task<IActionResult> GetRequestByTenandId(int tenandId)
+        {
+            return Ok(await _requestServce.GetRequestsByTenandId(tenandId));
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> GetGetRequestsByPropertyId(int propertyId)
+        {
+            return Ok(await _requestServce.GetRequestsByPropertyId(propertyId));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRequest([FromBody] RequestCreateDto requestDto)
+        {
+            var newRequest = await _requestServce.CreateRequest(requestDto);
+            return CreatedAtRoute("GetRequestById", new { id = newRequest.Id }, newRequest);
+        }
+    }
+}
