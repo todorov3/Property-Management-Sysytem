9ed736e66e9259b4a686db005cc85af9849746dd
todorov3 <yuliantodorovit@gmail.com>
2023-10-18 17:40:20 +0300

    Update RequestApiController

--- PropertyManagementSystem/PropertyManagementSystem/Controllers/RequestApiController.cs
+++ PropertyManagementSystem/PropertyManagementSystem/Controllers/RequestApiController.cs
@@ -8,42 +8,43 @@ namespace PropertyManagementSystem.Controllers
     [Route("api/requests")]
     public class RequestApiController : ControllerBase
     {
-        private readonly IRequestService _requestServce;
+        private readonly IRequestService _requestService;
 
         public RequestApiController(IRequestService requestService)
         {
-            _requestServce = requestService;
+            _requestService = requestService;
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("request/{id}")]
         public async Task<IActionResult> GetRequestById(int id)
         {
-            return Ok(await _requestServce.GetRequestById(id));
+            return Ok(await _requestService.GetRequestById(id));
         }
 
-        [HttpGet("{tenandId}")]
+        [HttpGet("tenand/{tenandId}")]
         public async Task<IActionResult> GetRequestByTenandId(int tenandId)
         {
-            return Ok(await _requestServce.GetRequestsByTenandId(tenandId));
+            return Ok(await _requestService.GetRequestsByTenandId(tenandId));
         }
 
-        [HttpGet("{propertyId}")]
+        [HttpGet("property/{propertyId}")]
         public async Task<IActionResult> GetRequestsByPropertyId(int propertyId)
         {
-            return Ok(await _requestServce.GetRequestsByPropertyId(propertyId));
+            return Ok(await _requestService.GetRequestsByPropertyId(propertyId));
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateRequest([FromBody] RequestCreateDto requestDto)
         {
-            var newRequest = await _requestServce.CreateRequest(requestDto);
-            return CreatedAtRoute("GetRequestById", new { id = newRequest.Id }, newRequest);
+            var newRequest = await _requestService.CreateRequest(requestDto);
+            //return CreatedAtRoute("GetRequestById", new { id = newRequest.Id }, newRequest);
+            return Ok(newRequest);
         }
 
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteRequest(int id)
         {
-            await _requestServce.DeleteRequest(id);
+            await _requestService.DeleteRequest(id);
             return Ok();
         }
     }
