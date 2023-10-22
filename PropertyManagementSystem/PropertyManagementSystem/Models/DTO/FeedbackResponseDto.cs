e54a74186f2b03f314a0068dd41fbf46f2216f6e
todorov3 <yuliantodorovit@gmail.com>
2023-10-22 14:42:53 +0300

    Add FeedbackResponseDto, Update RequestCreateDto

---
+++ PropertyManagementSystem/PropertyManagementSystem/Models/DTO/FeedbackResponseDto.cs
@@ -0,0 +1,14 @@
+﻿namespace PropertyManagementSystem.Models.DTO
+{
+    public class FeedbackResponseDto
+    {
+        public int Id { get; set; }
+        public int RequestId { get; set; }
+        public string Content { get; set; }
+        public int AuthorId { get; set; }
+        public int CommentedUserId { get; set; }
+        public bool IsAuthorLandlord { get; set; }
+        public int Rating { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
--- PropertyManagementSystem/PropertyManagementSystem/Models/DTO/RequestCreateDto.cs
+++ PropertyManagementSystem/PropertyManagementSystem/Models/DTO/RequestCreateDto.cs
@@ -4,8 +4,8 @@
     {
         public int TenandId { get; set; }
         public int PropertyId { get; set; }
-        public DateOnly MoveIn { get; set; }
-        public DateOnly MoveOut { get; set; }
-        public bool? IsAccepted { get; set; }
+        public DateTime MoveIn { get; set; }
+        public DateTime MoveOut { get; set; }
+        public int IsAccepted { get; set; }
     }
 }
